1ea40310beaaa95f8c528d3e6b7b0a4c174ba4e7
ZoriBak <167759171+ZoriBak@users.noreply.github.com>
2025-08-20 09:10:02 +0200

    Update db.php

--- news-website/config/db.php
+++ news-website/config/db.php
@@ -8,11 +8,10 @@ $pass = 'root'; // Database password
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4"; 
 
 try { 
-    $pdo = new PDO($dsn, $user, $pass); // Create a new Php date object instance
+    $pdo = new PDO($dsn, $user, $pass); 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 } catch (PDOException $e) { // connection errors
-    die("Database connection failed: " . $e->getMessage()); // Stop and print error
-}
+    die("Database connection failed: " . $e->getMessage()); 
 
 
 
